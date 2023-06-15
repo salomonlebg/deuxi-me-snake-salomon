ab4260b6094e4f454ebd8f13cb63c6841270156a
Salomon <salomon.graber@edu.gbjb.ch>
2023-06-15 03:16:18 +0200

    deuxième snake

--- Deuxieme snake.py
+++ Deuxieme snake.py
@@ -5,33 +5,34 @@ canvas = Canvas(tk,width = 600, height = 600 , bd=0, bg="darkblue")
 canvas.pack(padx=10,pady=10)
 #taille du canvas etc
 
-serp1 = canvas.create_rectangle(0, 0, 50, 50,fill='red')
-(serp1Left, serp1Top, serp1Right, serp1Bottom) = canvas.coords(serp1)
+Pos_X=275
+Pos_Y=450
+#position de la tête au début
 
+dx=0
+dy=0
+#déplacement de la tête au début
 
+serp1 = canvas.create_rectangle(Pos_X,Pos_Y,Pos_X+50,Pos_Y+50,fill='red')
+#taille de la tête, couleur etc
 
 def deplacement():
     global dx,dy
+    if canvas.coords(serp1)[3]>600:
+        close()
+    elif canvas.coords(serp1)[0]<0:
+        close()
+    elif canvas.coords(serp1)[1]<0:
+        close()
+    elif canvas.coords(serp1)[2]>600:
+        close()
     canvas.move(serp1,dx,dy)
     tk.after(20,deplacement)
 
-def check_game_over():
-    (serp1Left, serp1Top, serp1Right, serp1Bottom) = canvas.coords(serp1)
-    if serp1Bottom > 600:
-        close()
-    elif serp1Top < 0:
-        close()
-    elif serp1Left < 0:
-        close()
-    elif serp1Right > 600:
-        close()
-
-
-
+   
 def close():
-    global windowOpen
-    windowOpen = False
-    window.destroy()
+    tk.destroy()
+# si la tête touche le bord, le fenetre se ferme
 
 
 
@@ -57,23 +58,14 @@ def bas(event):
 #mouvement en bas    
 
 
-#position de la tête au début
-
-dx=0
-dy=0
-#déplacement de la tête au début
-
-
-
 canvas.bind_all('<Right>', droite)
 canvas.bind_all('<Left>', gauche)
 canvas.bind_all('<Up>', haut)
 canvas.bind_all('<Down>', bas)
+#binding des flèches du clavier
+
 
 deplacement()
-
-
-
 
 tk.mainloop()
 
