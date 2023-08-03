be3c89eb47448d9eed9b93f5e7397d2c3d8b11f2
Salomon <salomon.graber@edu.gbjb.ch>
2023-08-04 00:51:28 +0200

    modif esthétiques

--- Deuxieme snake.py
+++ Deuxieme snake.py
@@ -1,60 +1,69 @@
 from tkinter import*
+import random
 
 tk = Tk()
-canvas = Canvas(tk,width = 600, height = 600 , bd=0, bg="darkblue")
+canvas = Canvas(tk,width = 599, height = 599 , bd=0, bg="darkblue")
 canvas.pack(padx=10,pady=10)
 #taille du canvas etc
 
-Pos_X=275
+Pos_X=250
 Pos_Y=450
 #position de la tête au début
 
-dx=0
+dx=50
 dy=0
 #déplacement de la tête au début
 
 serp1 = canvas.create_rectangle(Pos_X,Pos_Y,Pos_X+50,Pos_Y+50,fill='red')
 #taille de la tête, couleur etc
 
+score = 0
+
 def deplacement():
     global dx,dy
-    if canvas.coords(serp1)[3]>600:
+    if canvas.coords(serp1)[2]>599:
         close()
-    elif canvas.coords(serp1)[0]<0:
+    elif canvas.coords(serp1)[0]<1:
         close()
-    elif canvas.coords(serp1)[1]<0:
+    elif canvas.coords(serp1)[1]<1:
         close()
-    elif canvas.coords(serp1)[2]>600:
+    elif canvas.coords(serp1)[3]>599:
         close()
     canvas.move(serp1,dx,dy)
-    tk.after(20,deplacement)
+    tk.after(235,deplacement)
 
-   
+
+      
 def close():
-    tk.destroy()
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2,
+                       canvas.winfo_height()/2,
+                       font=('consolas', 45),
+                       text="PARTIE TERMINÉE !", fill="red")
+                       
+    print(" Tu as mangé " + str(score)+" pommes ")
+   
 # si la tête touche le bord, le fenetre se ferme
-
-
 
 def droite(event):
     global dx,dy
-    dx=5
+    dx=50
     dy=0
 #mouvement a droite
 def gauche(event):
     global dx,dy
-    dx=-5
+    dx=-50
     dy=0
 #mouvement a gauche
 def haut(event):
     global dx,dy
     dx=0
-    dy=-5
+    dy=-50
 #mouvement en haut
 def bas(event):
     global dx,dy
     dx=0
-    dy=5
+    dy=50
 #mouvement en bas    
 
 
@@ -63,7 +72,6 @@ canvas.bind_all('<Left>', gauche)
 canvas.bind_all('<Up>', haut)
 canvas.bind_all('<Down>', bas)
 #binding des flèches du clavier
-
 
 deplacement()
 
