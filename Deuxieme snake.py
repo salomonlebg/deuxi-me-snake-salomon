26e21fa5bdcb2b22e4bbf7a0aebffe6c7e18ba68
Salomon <salomon.graber@edu.gbjb.ch>
2023-10-13 02:48:57 +0200

    jeu qui fonctionne

--- Deuxieme snake.py
+++ Deuxieme snake.py
@@ -3,48 +3,51 @@ import random
 
 score = 0
 
-# vitesse du sepend
-speed=500
+# vitesse du serpent
+speed=300
 
-m = 50
-n = 12
+#taille d'un carré
+tc = 50
 
-#x = [0,50,100,150,200,250,300,350,400,450,500,550,600]
-x =  range(0,n*m,m)
+#vitesse d'un carré
+v = 50
 
+#nombre de carrés
+nc = 12
 
-dx = [m,m,m]
+# liste des directions de chaque carrés
+dx = [v,v,v]
 dy = [0,0,0]
 
 
 #taille du canvas etc
 tk = Tk()
-canvas = Canvas(tk,width = 600, height = 600 , bd=0, bg="darkblue")
+canvas = Canvas(tk,width = nc*tc, height = nc*tc , bd=0, bg="darkblue")
 canvas.pack(padx=10,pady=10)
 
 
 #position de la tête au début
-Pos_X=x[5]
-Pos_Y=x[9]
+Pos_X=5*tc
+Pos_Y=9*tc
 
 
 #liste des rectangle du serpent
-serp = [canvas.create_rectangle(Pos_X,Pos_Y,Pos_X+m,Pos_Y+m,fill='darkgreen'),
-        canvas.create_rectangle(Pos_X-m,Pos_Y,Pos_X,Pos_Y+m,fill='green'),
-        canvas.create_rectangle(Pos_X-2*m,Pos_Y,Pos_X-m,Pos_Y+m,fill='green')]
+serp = [canvas.create_rectangle(Pos_X,Pos_Y,Pos_X+tc,Pos_Y+tc,fill='darkgreen'),
+        canvas.create_rectangle(Pos_X-tc,Pos_Y,Pos_X,Pos_Y+tc,fill='green'),
+        canvas.create_rectangle(Pos_X-2*tc,Pos_Y,Pos_X-tc,Pos_Y+tc,fill='green')]
 
 # détections si la tête touche le bord du canvas
 # déplacement de la tête + des corps
-# on vérifie les coordonées de la tête pour la collision
+# on vérifie les coordonées de la tête pour la collision avec la pomme et le corps 
 def deplacement():
     global dx,dy
-    if canvas.coords(serp[0])[2]>600:
+    if canvas.coords(serp[0])[2]>nc*tc:
         close()
     elif canvas.coords(serp[0])[0]<0:
         close()
     elif canvas.coords(serp[0])[1]<0:
         close()
-    elif canvas.coords(serp[0])[3]>600:
+    elif canvas.coords(serp[0])[3]>nc*tc:
         close()
     else :
         for k in range(0,len(dx)):
@@ -53,54 +56,65 @@ def deplacement():
             dx[k] = dx[k-1]
             dy[k] = dy[k-1]
         collision()
+        for i in range(len(dx)-1,0,-1):
+            if canvas.coords(serp[0]) == canvas.coords(serp[i]):
+                close()
         tk.after(speed,deplacement)
- # si la tête touche le bord, le fenetre se ferme
 
-ma_liste = [0,50,100,150,200,250,300,350,400,450,500,550]
+#coordonnés possibles de la pomme
+cp = [0,50,100,150,200,250,300,350,400,450,500,550]
+
+#création de la pomme sans la faire apparaître
 def créer_pomme():
-    x = random.choice(ma_liste)
-    y = random.choice(ma_liste)
+    x = random.choice(cp)
+    y = random.choice(cp)
     p = canvas.create_rectangle(x,y,x+50,y+50,fill='red')
     return(p)
 
 pomme1 = créer_pomme()
 
-
-c1,c2,c3,c4 = canvas.coords(serp[-1])
-
-# quand la tête touche la pomme
+# collision de la pomme et du serpent
+# apparition d'une nouvelle pomme
+# si la pomme apparait sur le corps du serpent, on en créer une nouvelle
 def collision():
     global pomme1
     global score
-    if canvas.coords(serp[0]) == canvas.coords(pomme1):
-        canvas.delete(pomme1)
-        pomme1 = créer_pomme()
-        score = score + 1
-        serp.append(canvas.create_rectangle(c1-dx[-1],c2-dy[-1],c3-dx[-1],c4-dy[-1],fill="green"))
-        dx.append(dx[-1])
-        dy.append(dx[-1])
-        
+    c1,c2,c3,c4 = canvas.coords(serp[-1])
+    for p in range(len(dx)-1,0,-1):
+        if canvas.coords(serp[p]) == canvas.coords(pomme1):
+                canvas.delete(pomme1)
+                pomme1 = créer_pomme()
+        elif canvas.coords(serp[0]) == canvas.coords(pomme1):
+            canvas.delete(pomme1)
+            pomme1 = créer_pomme()
+            score = score + 1
+            serp.append(canvas.create_rectangle(c1-dx[-1],c2-dy[-1],c3-dx[-1],c4-dy[-1],fill="green"))
+            dx.append(dx[-1])
+            dy.append(dy[-1])
+            if len(serp)-1 == 144:
+                victoire()
+            
 
 # actions des flèches directionelles         
 def droite(event):
     global dx,dy
-    dx[0]=x[1]
-    dy[0]=x[0]
+    dx[0]=v
+    dy[0]=0
 #mouvement a droite
 def gauche(event):
     global dx,dy
-    dx[0]=-m
-    dy[0]=x[0]
+    dx[0]=-v
+    dy[0]=0
 #mouvement a gauche
 def haut(event):
     global dx,dy
-    dx[0]=x[0]
-    dy[0]=-m
+    dx[0]=0
+    dy[0]=-v
 #mouvement en haut
 def bas(event):
     global dx,dy
-    dx[0]=x[0]
-    dy[0]=x[1]
+    dx[0]=0
+    dy[0]=v
 #mouvement en bas
 
 
@@ -115,7 +129,21 @@ def close():
                        canvas.winfo_height()/1.5,
                        font=('consolas', 20),
                        text="Bravo! tu as mangé " + str(score)+" pommes ", fill="red")
-    
+
+
+# ce qu'il se passe quand le serpent mange toutes les pommes
+def victoire():
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2,
+                       canvas.winfo_height()/2,
+                       font=('consolas', 40),
+                       text="BRAVO TU AS GAGNÉ,E !", fill="red")
+    canvas.create_text(canvas.winfo_width()/2,
+                       canvas.winfo_height()/1.5,
+                       font=('consolas', 20),
+                       text="Tu as mangé toutes les pommes", fill="red")
+
+
                        
 #binding des flèches du clavier
 canvas.bind_all('<Right>', droite)
